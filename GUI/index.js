fc05742243e444509aee61ad5110e25a22eb71c5
KevoHoff <67078785+KevoHoff@users.noreply.github.com>
2021-10-14 17:05:30 -0400

    Update index.js
    Fixed server-side posting, getting, and storing temporary data on the server.

--- GUI/index.js
+++ GUI/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json({limit:'5mb'}));
 
-app.post('/api/request', function (request, response) {
+var req_info = {}
+var current_req = 1
+
+app.post('/api/request', async function (request, response) {
     let data = request.body;
     console.log(data);
-    response.status(200).send(JSON.stringify(data));
+    req_info = data
+    response.status(200).send(data);
 })
-app.get('/temp.html', function (request, response) {
-    let data = request.body;
-    console.log(request.body);
-    response.json(data);
+app.get('/api/request', async function (request, response) {
+    let data = {}
+    console.log(current_req);
+    if (request.query.req_num == current_req) {
+        data = req_info
+        current_req++
+        console.log(data);
+    }
+    else {
+        console.log('Not expected request')
+    }
+    response.send(JSON.stringify(data));
 });
 
 app.listen(3000, () => console.log('listening at 3000'));
