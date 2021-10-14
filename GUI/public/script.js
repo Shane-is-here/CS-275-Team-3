c93402a9a40eba7e2af61c8d8ef69417c6e7299f
KevoHoff <67078785+KevoHoff@users.noreply.github.com>
2021-10-14 17:43:48 -0400

    Update script.js
    Fixed formatting of date-time: 'YYYY/MM/DD/hr/min'.

--- GUI/public/script.js
+++ GUI/public/script.js
@@ -5,6 +5,8 @@ let name = document.querySelector('.name');
 let start = document.querySelector('.start');
 let finish = document.querySelector('.finish');
 
+var reqNum = 0
+
 submit.addEventListener('click', async () => {
     let fname = document.getElementById('fname');
     let lname = document.getElementById('lname');
@@ -15,43 +17,56 @@ submit.addEventListener('click', async () => {
     let h2 = document.querySelector('.h2');
     block.style.display = "block";
     block.style.display = "none";
+
+    reqNum = reqNum + 1;
     
     /* Date: YYYY/mm/dd */
-    var d = new Date();
-    var YYYY = d.getFullYear();
-    var mm = d.getMonth();
-    var dd = d.getDay();
-    var hh = d.getHours();
-    var min = d.getMinutes();
-
-    if(mm < 10) {
-        mm = "0" + mm;
-    }
-    if(dd < 10) {
-        dd = "0" + dd;
-    }
+    let d = new Date();
+    let YYYY = d.getFullYear();
+    let mm = d.getMonth();
+    let dd = d.getDay();
+    let hh = d.getHours();
+    let min = d.getMinutes();
 
     let date_string = YYYY + "/" + mm + "/" + dd;
-    let time = hh + ":" + min;
-       
- 
+    let time = hh + "/" + min;
+
+    const data = {
+        request: 0,
+        first: fname.value,
+        last: lname.value,
+        phone: pnum.value,
+        start_time: date_string + '/' + time,
+        ID: id,
+        req_num: reqNum
+    };
+
+    post('/api/request', data);
+
     var id = Math.floor(Math.random()*100000);
     mod.style.display =  "block";
     let spots = ['G14', 'A12', 'C05'];
     var index = Math.floor(Math.random() * spots.length);
-    const nam = "Kevin Hoffman";
     spot.innerHTML = spots[index];
     name.innerHTML = "<b>Full name: </b>" + fname.value + " " + lname.value;
     phone.innerHTML = "<b>Phone Number: </b>" + pnum.value;
     start.innerHTML = "<b>Start Time: </b>" + date_string + " " + time;
     h2.innerHTML = "<b>ID: </b>" + id;
+
+    
     // finish.innerHTML = "<b>Expected Finish Time: </b>" + "Kevin Hoffman"; 
 });
 
-close.addEventListener('click', () => {
+close.addEventListener('click', async () => {
     let mod = document.querySelector('.modal');
     mod.style.display = "none";    
-    $.get("http://127.0.0.1:5500/temp.html", function(data) {
-        console.log(data);
-    });
+
+    fetch('/api/request?req_num=1')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
 });
