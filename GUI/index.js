c04ad9425acb5fea85d432b656c885dbb73c4ad5
KevoHoff <67078785+KevoHoff@users.noreply.github.com>
2021-10-14 17:34:57 -0400

    Update index.js
    Added communication routing from the Middleware to GUI direction through route '/api/receive'.

--- GUI/index.js
+++ GUI/index.js
@@ -7,20 +7,22 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json({limit:'5mb'}));
 
-var req_info = {}
+var user_request = {}
+var middle_post = {}
 var current_req = 1
 
+// Communication from the GUI to the Middleware
 app.post('/api/request', async function (request, response) {
     let data = request.body;
     console.log(data);
-    req_info = data
+    user_request = data
     response.status(200).send(data);
 })
 app.get('/api/request', async function (request, response) {
     let data = {}
     console.log(current_req);
     if (request.query.req_num == current_req) {
-        data = req_info
+        data = user_request
         current_req++
         console.log(data);
     }
@@ -30,5 +32,27 @@ app.get('/api/request', async function (request, response) {
     response.send(JSON.stringify(data));
 });
 
+// Communication from the Middleware to the GUI
+app.post('/api/receive', async function (request, response) {
+    let data = request.body;
+    console.log(data);
+    middle_post = data
+    response.status(200).send(data);
+})
+app.get('/api/receive', async function (request, response) {
+    let data = {}
+    console.log(current_req);
+    if (request.query.req_num == current_req) {
+        data = middle_post
+        current_req++
+        console.log(data);
+    }
+    else {
+        console.log('Not expected request')
+    }
+    response.send(JSON.stringify(data));
+});
+
+
+
 app.listen(3000, () => console.log('listening at 3000'));
-    
